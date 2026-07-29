e06ca7cf2ce245c872169d692a932ba3458b4cac
Kurt <kurt_russel_villapando@dlsl.edu.ph>
2026-07-29 15:21:16 +0800

    GT5: Add Express server with routings

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import { mockTasks } from '../src/utils.js';
-import { fetchSampleUsers } from '../src/api.js';
+import { mockTasks } from '../utils.js';
+import { fetchSampleUsers } from '../api.js';
 
 const router = express.Router();
 
