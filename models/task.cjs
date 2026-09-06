b8ac78fe26c554d09071e46bb4ee55b3f13579e7
Kurt <kurt_russel_villapando@dlsl.edu.ph>
2026-09-06 17:30:34 +0800

    Changes for midterms

--- models/task.cjs
+++ models/task.cjs
@@ -4,21 +4,20 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
       Task.belongsTo(models.User, { foreignKey: 'userId' });
     }
   }
   Task.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATE,
-    completed: DataTypes.BOOLEAN,
-    userId: DataTypes.INTEGER
-  }, {
+  title: DataTypes.STRING,
+  dueDate: DataTypes.DATE,
+  completed: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false
+  },
+  userId: DataTypes.INTEGER,
+}, {
     sequelize,
     modelName: 'Task',
   });
--- src/api.js
+++ src/api.js
@@ -27,3 +27,5 @@ export function fetchSampleUsersPromise() {
       return [];
     });
 }
+
+//CHANGE TO PUSH
